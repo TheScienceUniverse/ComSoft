23a11f73ef5683bc65e0433014115bcb42462fe7
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-06-09 03:23:44 +0530

    Add files via upload

---
+++ Code2WebCode/Demo/C2HTM/code.c
@@ -0,0 +1,10 @@
+/*
+ * Basic Hello World code in C
+ */
+// Printing Hello World
+#include <stdio.h>
+int main(int argc, char *argv[]){
+	float x = 10.00;
+	printf("Hello, World!\n");
+	return 0;
+}
